38c36232dc5918eab948f395d413776a4a204cf0
Ashwin Naren <arihant2math@gmail.com>
2024-03-12 11:12:07 -0700

    bugfixes

--- pose_service_client_node.py
+++ pose_service_client_node.py
@@ -9,7 +9,7 @@ from networktables import NetworkTables
 class PoseServiceClient(Node):
 
     def __init__(self):
-        super().__init__('minimal_client_async')
+        super().__init__('get_all_poses_client_async')
         self.cli = self.create_client(GetAllPoses, 'get_all_poses')
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
@@ -20,8 +20,8 @@ class PoseServiceClient(Node):
         # self.sd.putNumber('someNumber', 1234)
         # otherNumber = self.sd.getNumber('otherNumber')
 
-    def send_request(self, a, b):
-        self.req.max_count = 4
+    def send_request(self, max_count):
+        self.req.max_count = max_count
         self.future = self.cli.call_async(self.req)
         rclpy.spin_until_future_complete(self, self.future)
         return self.future.result()
@@ -30,12 +30,13 @@ class PoseServiceClient(Node):
 def serialize_response(poses):
     return ""
 
+
 def main(args=None):
     rclpy.init(args=args)
 
     client = PoseServiceClient()
     while True:
-        response = client.send_request(int(sys.argv[1]), int(sys.argv[2]))
+        response = client.send_request(4)
         client.sd.putBoolean("vslam_success", response.success)
         client.sd.putString("vslam_pose", serialize_response(response.poses))
         if not client.sd.getBoolean("vslam_running"):
