c0af5e878a36423daad5b3096361f377f36f5a0d
Ashwin Naren <arihant2math@gmail.com>
2024-03-13 15:58:17 -0700

    update

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'comms_node'
@@ -8,8 +11,9 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
