0066d10b9adf5117a18d164e655dec2ce4928f6e
Ashwin Naren <arihant2math@gmail.com>
2024-03-13 21:09:22 -0700

    added setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ ros-humble-isaac-ros-detectnet ros-humble-isaac-ros-triton ros-humble-isaac-ros-
 cd /workspaces/isaac_ros-dev && \
 colcon build --symlink-install && \
 source install/setup.bash
-ros2 launch comms_node run_network_tables.launch.py
+python3 -m pip install -U pynetworktables
