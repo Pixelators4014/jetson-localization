2517bb3cf4132135102343b040ef8f74cf458d3e
Alistair Keiller <alistair@keiller.net>
2024-03-14 19:29:51 -0700

    test

--- comms_node/detect_subscriber.py
+++ comms_node/detect_subscriber.py
@@ -17,7 +17,9 @@ class DetectSubscriber(Node):
         self.get_logger().info("test")
         for detection in msg.detections:
             self.get_logger().info(
-                f'Detected: {detection.bbox.center.x:.2f} {detection.bbox.center.y:.2f} {detection.bbox.size_x:.2f} {detection.bbox.size_y:.2f}')
+                # f"{detection.bbox.center.x:.2f}, {detection.bbox.center.y:.2f}, {detection.bbox.size_x:.2f}, {detection.bbox.size_y:.2f}"
+                f"{detection.bbox.size_x:.2f}, {detection.bbox.size_y:.2f}"
+            )
 
 
 def main(args=None):
