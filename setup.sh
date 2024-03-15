c4ad55895c74b31f16918388c9737bbda1be53ce
Ashwin Naren <arihant2math@gmail.com>
2024-03-14 21:35:06 -0700

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/sh
 sudo apt-get install -y ros-humble-isaac-ros-visual-slam \
 ros-humble-isaac-ros-yolov8 ros-humble-isaac-ros-tensor-rt ros-humble-isaac-ros-dnn-image-encoder \
-ros-humble-isaac-ros-detectnet ros-humble-isaac-ros-triton ros-humble-isaac-ros-dnn-image-encoder && \
+ros-humble-isaac-ros-detectnet ros-humble-isaac-ros-triton ros-humble-isaac-ros-dnn-image-encoder \
+ros-humble-isaac-ros-apriltag && \
 cd /workspaces/isaac_ros-dev && \
 colcon build --symlink-install && \
 source install/setup.bash
